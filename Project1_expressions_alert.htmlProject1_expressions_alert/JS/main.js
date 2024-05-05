ada5b34c91ad840a464c640277930f1a64cdce2a
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-05 18:24:20 +0100

    edited

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,4 +1,4 @@
-<script src="main.js"></script>
+
 <script>
     alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 </script>
