02b5d647a4fe195344f39aa3252439979aeb906a
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-05 18:13:46 +0100

    script tag added

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,2 +1,4 @@
 <script src="main.js"></script>
-alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+<script>
+    alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+</script>
