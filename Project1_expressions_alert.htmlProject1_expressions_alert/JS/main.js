0e0e15a41ba854c0ac3ed301fa63870717ce7646
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-06 09:01:22 +0100

    method window.alert("");

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,4 +1,4 @@
 
-    alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+    window.alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 
 
--- Project1_expressions_alert.htmlProject1_expressions_alert/README.md
+++ Project1_expressions_alert.htmlProject1_expressions_alert/README.md
@@ -2,3 +2,4 @@
  <p>Basics of the programming language, JavaScript.</p>
 <p>Starting 2nd year training</p>
 <p>Folder created, script alert in index.html file</p>
+<p>method: alert("x"); replaced with window.alert("x");
