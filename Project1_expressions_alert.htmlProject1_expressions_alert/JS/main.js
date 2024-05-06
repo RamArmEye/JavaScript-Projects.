4989314622c3e34bb03f3bd7abd6bdce72f60cf5
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-06 16:30:56 +0100

    using backslashes

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -6,8 +6,6 @@ document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mund
 var S = "This is string";
 document.write(S);
 --
-*/
-
 var name = "Jim Carrey",
     age = "55",
     height = "5'9\"",
@@ -15,3 +13,7 @@ var name = "Jim Carrey",
     eye = "blue";
 
 window.alert("Name: " + name + "\nAge: " + age + "\nHeight: " + height + "\nHair: " + hair + "\nEye: " + eye);
+--
+*/
+document.write("Lisa told Bart,\"Knock it off, Bart! Or I'll tell dad!\
+"<br>\"Eat my shorts!\"Bart responded.");
--- Project1_expressions_alert.htmlProject1_expressions_alert/README.md
+++ Project1_expressions_alert.htmlProject1_expressions_alert/README.md
@@ -1,8 +1,9 @@
 # JavaScript-Projects.
  <h3>Basics of the programming language, JavaScript.</h3>
-<h1>Starting 2nd year training</h1>
+<h5>Starting 2nd year training</h5>
 <p>Folder created, script alert in index.html file</p>
 <p>method: alert("x"); replaced with window.alert("x");</p>
 <p>method window.alert("x"); replaced with document.write("x");</p>
 <p>testing variable</p>
 <p>String assignment</p>
+<p>adding backslashes</p>
