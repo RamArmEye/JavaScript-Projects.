55effdf29c85a01ae5c9e18b99703aae462fd758
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-06 09:29:52 +0100

    method document.write("x");

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,8 +1,8 @@
 /*
 alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 window.alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
-
+*/
 
 document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
-*/
-document.write("Hello World!");
+
+
