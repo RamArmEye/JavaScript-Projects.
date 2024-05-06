679c78535736f9b1fc69a45768ba3868fc6dcec9
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-06 16:11:07 +0100

    testing variables

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,8 +1,7 @@
 /*
 alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 window.alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
-*/
-
 document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
-
-
+*/
+var S = "This is string";
+document.write(S);
