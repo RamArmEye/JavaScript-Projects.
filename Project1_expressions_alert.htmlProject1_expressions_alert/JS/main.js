027cb266e18644e4c79a053d7c080093384180bd
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-06 09:28:14 +0100

    testing single line

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -1,6 +1,8 @@
 /*
 alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 window.alert("English: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
-*/
-document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
 
+
+document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+*/
+document.write("Hello World!");
--- Project1_expressions_alert.htmlProject1_expressions_alert/README.md
+++ Project1_expressions_alert.htmlProject1_expressions_alert/README.md
@@ -2,5 +2,5 @@
  <p>Basics of the programming language, JavaScript.</p>
 <p>Starting 2nd year training</p>
 <p>Folder created, script alert in index.html file</p>
-<p>method: alert("x"); replaced with window.alert("x");
-method window.alert("x"); replaced with document.write("x");
+<p>method: alert("x"); replaced with window.alert("x");</p>
+<p>method window.alert("x"); replaced with document.write("x");</p>
