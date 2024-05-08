cfa790c0e55f968a39c49bb3e2add9b569b86587
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-08 21:18:34 +0100

    Basic Java Sript Projects Assignment

--- Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
+++ Project1_expressions_alert.htmlProject1_expressions_alert/JS/main.js
@@ -46,16 +46,15 @@ var name = "Jim Carrey",
 //Basic Java Sript Projects Assignment
 
 // window.alert and document.write methods
-window.alert("Method:window.alert\nEnglish: Hello world");
-
-document.write("Method: document.write\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+window.alert("Method: window.alert\nEnglish: Hello world\nSpanish: Hola mundo\nFrench: Bonjour monde\nGerman: Hallo Welt\nLithuanian: Labas pasauli\nPortuguese: Olá mundo");
+document.write("Method: document.write\English: Hello world");
 
 // Variable 'Sent1'
 var Sent1 = "This is the beginning of the string";
 // Variable 'Sent2' 
 var Sent2 = " and this is the end of the string";
 // Full sentence
-document .write(Sent1 + Sent2);
+window.alert(Sent1 + Sent2);
 
 // Example expression: 
 var name = "Jim Carrey",
