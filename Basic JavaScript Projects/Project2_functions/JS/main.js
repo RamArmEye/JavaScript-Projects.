1189d237993cedbe4f76d26e67238052cd5b3fa3
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-09 10:08:06 +0100

    Function assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,8 +1,8 @@
 //after clicking the button two variables become as single string
-function clickButton() {   
-// Variable 'Sent1'
-    var Sent1 = "This is the beginning of the string";
-// Variable 'Sent2' 
-    var Sent2 = " and this is the end of the string";
-    document.getElementById("concatenate").textContent = Sent1 + Sent2;
+function Click_Button() {   
+    // Variable 'Sent1'
+        var Sent1 = "This is the beginning of the string";
+    // Variable 'Sent2' 
+        var Sent2 = " and this is the end of the string";
+        document.getElementById("concatenate").innerHTML = Sent1 + " " + Sent2;
 }
--- Basic JavaScript Projects/Project2_functions/README.md
+++ Basic JavaScript Projects/Project2_functions/README.md
@@ -1,7 +1,7 @@
 <h3>Basics of the programming language, JavaScript.</h3>
 <h5>Project2_functions</h5>
 <p>Created: readme.md index.html JS folder and main.js </p>
-<p></p>
+<p>Function assigment</p>
 <p></p>
 <p></p>
 <p></p>
