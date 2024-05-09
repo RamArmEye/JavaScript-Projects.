aec12695347dfb6c7c8f76aec685954ce2ae7522
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-09 19:03:30 +0100

    Multiple Operators Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,3 +1,4 @@
+/*
 //  1 Subtraction Multiplication and Division Assignment
 function subtraction() {function called after button clicked
 	var subtraction = 5-2; // variable called subtraction and equals 5-2
@@ -12,5 +13,12 @@ function multiplication() {
 function division() {
 	var division = 100/2;
 	document.getElementById("division").innerHTML = "100/2 = " + division;
-
+}
+*/
+--
+// 2 Multiple Operators Assignment
+function more_math() {
+    var result = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("math_result").innerHTML = 
+        "1 plus 2, multiplied by 10, divided in half, and then subtracted by 5 equals " + result;
 }
--- Basic JavaScript Projects/Project3_math_operators/README.md
+++ Basic JavaScript Projects/Project3_math_operators/README.md
@@ -2,7 +2,7 @@
 <h5>Project3_math_operators</h5>
 <p>Created: readme.md index.html JS folder and main.js </p>
 <p>1 Subtraction Multiplication and Division Assignment</p>
-<p>2 </p>
+<p>2 Multiple Operators Assignment</p>
 <p></p>
 <p></p>
 <p></p>
