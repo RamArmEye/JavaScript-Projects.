067fc51bc8b09a7e23c66e3e3170de8a4c557df6
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-09 08:13:12 +0100

    Function assignment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,8 @@
+//after clicking the button two variables become as single string
+function clickButton() {   
 // Variable 'Sent1'
-var Sent1 = "This is the beginning of the string";
+    var Sent1 = "This is the beginning of the string";
 // Variable 'Sent2' 
-var Sent2 = " and this is the end of the string";
-// Full sentence
-window.alert(Sent1 + Sent2)
+    var Sent2 = " and this is the end of the string";
+    document.getElementById("concatenate").textContent = Sent1 + Sent2;
+}
