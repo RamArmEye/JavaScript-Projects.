df101f9723536513b1ca7f98838f02770f95b7dc
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-09 16:49:43 +0100

    Subtraction Multiplication and Division Assignment

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,16 @@
-function clickButton() {
-	var substraction = 5-2;
-	document.getElementById("Math").innerHTML = "5-2 = " + substraction;
+
+function subtraction() {
+	var subtraction = 5-2;
+	document.getElementById("subtraction").innerHTML = "5-2 = " + subtraction;
+	
+}	
+function multiplication() {
+	var multiplication = 5*2;
+	document.getElementById("multiplication").innerHTML = "5*2 = " + multiplication;
+
+}
+function division() {
+	var division = 100/2;
+	document.getElementById("division").innerHTML = "100/2 = " + division;
+
 }
