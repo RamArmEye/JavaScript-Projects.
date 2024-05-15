a3a734bd34acb3c67802b3c9ba64f1da50efa56b
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-15 23:09:36 +0100

    Infinity Assignment

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,5 +1,5 @@
 // Project5_comparisons_type_coercion
-
+/*
 // 1 Type Of Operator and Coercion Assignment
 function dataType() {
     // Define a variable
@@ -9,3 +9,14 @@ function dataType() {
     document.write("Data type of myVariable is: "  +  typeof myVariable);
     document.write("10" + myVariable)
 }
+*/
+
+// 2 Infinity Assignment
+function posInfinity() {
+    var pInfinity = 23469629  // enter number to test infinity 
+    document.getElementById("Infinity").innerHTML = pInfinity;
+ }
+ function negInfinity() {
+    var pInfinity = (-2E310) 
+    document.getElementById("nInfinity").innerHTML = pInfinity;
+ }
--- Basic JavaScript Projects/Project5_comparisons_type_coercion/README.md
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/README.md
@@ -1,8 +1,7 @@
-<p>Project4_dictionaries</p>
+<p>Project5_comparisons_type_coercion</p>
 <p>Empty files created</p>
-<p> Project5_comparisons_type_coercion</p>
-<p>Type Of Operator and Coercion Assignment</p>
-<p></p>
+<p>1 Type Of Operator and Coercion Assignment</p>
+<p>2 Infinity Assignment</p>
 <p></p>
 <p></p>
 <p></p>
