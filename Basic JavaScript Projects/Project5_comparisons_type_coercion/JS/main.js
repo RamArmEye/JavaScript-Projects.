b7b3f2b6994f09d2426c75915dfae26015393b2e
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-15 22:18:25 +0100

    1 Type Of Operator and Coercion Assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,11 @@
+// Project5_comparisons_type_coercion
+
+// 1 Type Of Operator and Coercion Assignment
+function dataType() {
+    // Define a variable
+    var myVariable = 42;
+    
+    // Use document.write() method to display the data type
+    document.write("Data type of myVariable is: "  +  typeof myVariable);
+    document.write("10" + myVariable)
+}
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/README.md
@@ -0,0 +1,13 @@
+<p>Project4_dictionaries</p>
+<p>Empty files created</p>
+<p> Project5_comparisons_type_coercion</p>
+<p>Type Of Operator and Coercion Assignment</p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
