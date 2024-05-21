9f1b2cc4480a91c0d82cd417e78aed2aab14a4ca
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-21 06:24:45 +0100

    Project6_ternary__operators_constructors assignment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,7 +1,15 @@
 //Project6_ternary__operators_constructors
+// Function to check if the rider is tall enough to ride
 function Ride_Function() {   
+    // Declare variables for height and riding 
     var Height, Can_ride;
+
+    // Get the value from the input element with id 'Height'
     Height = document.getElementById("Height").value;
-    Can_ride = (Height < 53) ? "You are too short" : "You are tall enough";
+
+    // Use ternary operator to determine if the rider is tall enough
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+
+    // Display the result in the paragraph element with id 'Ride'
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
