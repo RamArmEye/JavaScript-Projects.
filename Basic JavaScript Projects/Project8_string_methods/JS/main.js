a432623440dda76a2f281ba792e0b14d7c48139e
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-22 18:36:56 +0100

    concat() Method AND slice() Method assignments

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,1 +1,14 @@
 
+//slice() Method
+function slice_Method() {
+    var sentence = "I have made this into a complete sentence";
+    var section = sentence.slice(0,1);
+    document.getElementById("Slice").innerHTML = section;
+}
+
+function slice_Method1() {
+    var sentence1 = "I can do it";
+    var section1 = sentence1.slice(2,5);
+    document.getElementById("Slice1").innerHTML = section1;
+}
+
--- Basic JavaScript Projects/Project8_string_methods/README.md
+++ Basic JavaScript Projects/Project8_string_methods/README.md
@@ -1,7 +1,7 @@
 <h3>Basics of the programming language, JavaScript.</h3>
 <h5>Project8_string_methods</h5>
-<p> Created standar templates: readme.md index.html JS folder and main.js </
-<p></p>
+<p> Created standar templates: readme.md index.html  JS folder and main.js 
+<p>concat() Method AND slice() Method assignments</p>
 <p></p>
 <p></p>
 <p></p>
