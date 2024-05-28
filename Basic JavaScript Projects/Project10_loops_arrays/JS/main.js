68a0e05b8a8475e08d47c2c8cf4e1ed0ed219ee0
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-05-28 22:57:46 +0100

    For Loop Assignment

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,6 @@
 // Project10_loops_arrays
+
+//Loop Assignment
 function Call_Loop() {
   var Digit = "";
   var X = 1;
@@ -7,4 +9,16 @@ function Call_Loop() {
     X++;
 }
 document.getElementById("Loop").innerHTML = Digit;
+}
+
+//For Loop Assignment
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+  for (Y = 0; Y < Instruments.length; Y++) {
+  Content += Instruments[Y] + "<br>";
+  }
+  document.getElementById("List_of_Instruments").innerHTML = Content;
 }
--- Basic JavaScript Projects/Project10_loops_arrays/README.md
+++ Basic JavaScript Projects/Project10_loops_arrays/README.md
@@ -1,7 +1,7 @@
 <h3>Basics of the programming language, JavaScript.</h3>
 <h5>Project10_loops_arrays</h5>
 <p> Created standar templates: readme.md index.html  JS folder and main.js </p>
-<p></p>
-<p></p>
+<p>Loop Assignment</p>
+<p>For Loop Assignment</p>
 <p></p>
 <p></p>
