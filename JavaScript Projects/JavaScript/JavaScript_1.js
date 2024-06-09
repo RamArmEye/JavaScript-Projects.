024db5e67fc6933aa4dc43b9c46a362faadc0aaf
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-06-09 23:49:31 +0100

    Switch Statement Assignment

---
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,35 @@
+// Function to handle color submission
+function Color_Function() {
+  // Output variable
+  var Color_Output;
+  // Get input value
+  var Colors = document.getElementById("Color_Input").value;
+  var Color_String = " is a great color!";
+  
+  // Switch case
+  switch(Colors) {
+      case "Red":
+          Color_Output = "Red" + Color_String;
+          break;
+      case "Yellow":
+          Color_Output = "Yellow" + Color_String;
+          break;
+      case "Green":
+          Color_Output = "Green" + Color_String;
+          break;
+      case "Blue":
+          Color_Output = "Blue" + Color_String;
+          break;
+      case "Pink":
+          Color_Output = "Pink" + Color_String;
+          break;
+      case "Purple":
+          Color_Output = "Purple" + Color_String;
+          break;
+      default:
+          Color_Output = "Please enter a color exactly as written on the above list.";
+  }
+  // Display output
+  document.getElementById("Output").innerHTML = Color_Output;
+}
+
---
+++ JavaScript Projects/README.md
@@ -0,0 +1,5 @@
+<h3>JavaScript Projects</h3>
+<h5>Switch Statement Assignment</h5>
+<p></p>
+<p></p>
+<p></p>
