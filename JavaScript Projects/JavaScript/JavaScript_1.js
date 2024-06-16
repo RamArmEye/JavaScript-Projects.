2b760771362733c356f5b041aceab2e711a17f80
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-06-16 05:09:40 +0100

    Canvas Element Assignment and Chalange

--- JavaScript Projects/JavaScript/JavaScript_1.js
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -38,3 +38,16 @@ function Hello_World_Function() {
   var A = document.getElementsByClassName("Click");
   A [0].innerHTML = "The text has changed!";
 }
+
+//HTML <canvas> Element and Chalange
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,150,80);
--- JavaScript Projects/README.md
+++ JavaScript Projects/README.md
@@ -1,5 +1,5 @@
 <h3>JavaScript Projects</h3>
 <h5>Switch Statement Assignment</h5>
 <p>document.getElementsByClassName() Method Assignment</p>
-<p>Canvas Element Assignment</p>
+<p>Canvas Element Assignment and Chalange</p>
 <p></p>
