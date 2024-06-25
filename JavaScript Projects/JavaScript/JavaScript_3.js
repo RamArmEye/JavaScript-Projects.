bb77ae5c154f383fbfe4fa203dae1ab5a693d8fd
RamArmEye <121702699+RamArmEye@users.noreply.github.com>
2024-06-25 04:18:34 +0100

    Data Attribute Assignment

--- JavaScript Projects/JavaScript/JavaScript_3.js
+++ JavaScript Projects/JavaScript/JavaScript_3.js
@@ -1,3 +1,4 @@
+//Data Attribute Assignment
 function displayType(character) {
     var characterType = character.getAttribute("data-character-type");
     alert(characterType + " is in the " + character.innerHTML + " universe!");
